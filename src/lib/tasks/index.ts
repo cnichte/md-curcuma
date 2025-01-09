54c8cf6221292c6ccae4cf4bb6e082a15b6fe32b
Carsten Nichte <c.nichte@t-online.de>
2025-01-09 14:05:25 +0100

    api broken-link-checker missing

--- src/lib/tasks/index.ts
+++ src/lib/tasks/index.ts
@@ -1,6 +1,8 @@
 export * from './mappings';
 export * from './markdown';
 
+export * from './broken-link-checker';
+
 export * from './Mapping_Task';
 export * from './NOP_Task';
 export * from './Observable_Abstract_TaskBase'
